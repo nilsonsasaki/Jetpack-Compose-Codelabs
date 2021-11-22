78f458556896f2bf235fbd5c313f498badf670d7
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-22 16:29:09 -0300

    added ImageList

--- app/src/main/java/com/nilsonsasaki/jetpackcomposelayout/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/jetpackcomposelayout/MainActivity.kt
@@ -3,31 +3,20 @@ package com.nilsonsasaki.jetpackcomposelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.nilsonsasaki.jetpackcomposelayout.ui.theme.JetpackComposeLayoutTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +25,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LazyList()
+                    ImageList()
                 }
             }
         }
@@ -102,7 +91,7 @@ fun LayoutsCodelab() {
                 },
                 actions = {
                     IconButton(onClick = { */
-/*TODO*//*
+/*Do nothing*//*
  }) {
                         Icon(
                             imageVector = Icons.Filled.Favorite,
@@ -126,6 +115,7 @@ fun BodyContent(modifier: Modifier = Modifier) {
 }
 */
 
+/* third part of the Codelab
 @Composable
 fun SimpleList() {
     // We save the scrolling position with this state that can also
@@ -165,5 +155,69 @@ fun LazyList(){
 fun LazyListPreview(){
     JetpackComposeLayoutTheme {
         LazyList()
+    }
+}*/
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList() {
+
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+
+
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ImageListPreview() {
+    JetpackComposeLayoutTheme {
+        ImageList()
     }
 }
